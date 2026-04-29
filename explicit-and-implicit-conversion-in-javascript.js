f6cd995c19630da94099e15f02d77073eeb41a4d
Anjana Sreekanth <anjanasreekanth@outlook.com>
2026-04-29 15:05:49 -0500

    Debugging Challenge

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,37 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+//Corrected Code with Comments.
 
-let isValid = Boolean("false");
+let result = 5 - 2;// 5 is number so no need of " ".
+console.log ("The result is: " + result);
+
+let isValid = false; //If you simply want the variable to be false, assign the boolean literal directly without quotes
 if (isValid) {
-    console.log("This is valid!");
+  console.log("This is valid!");
+}
+else {
+  console.log ("This is invalid");
 }
 
-let age = "25";
+let age = 25; // since age is number no need to gibe in double quotes which makes it a string.
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+//One example of implicit type conversion using null.
+
+let firstNumber = 10;
+let secondNumber = null;
+let sum = firstNumber + secondNumber;
+console.log(sum);
+
+//One example of explicit type conversion.
+
+let stringValue = "55"; //"55" is given as string
+console.log(stringValue + 10);// output will be 5510 since "55" is defined as a string
+let numericValue = Number(stringValue); // converts "55" to number 55.
+console.log(numericValue);
+ console.log(numericValue + 10);// output will be 65 since "55" is converted to number 55
+ 
+
+
